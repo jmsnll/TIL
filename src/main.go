b1d1d8a9f402c71b71f60989301fad778b9971f3
James Neill <jamesneill@protonmail.com>
2020-04-23 00:19:00 +0100

    [src] Refactored  and  into a single class  which is populated resurively, allowing for many levels of directories
    Signed-off-by: James Neill <jamesneill@protonmail.com>

--- src/main.go
+++ src/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"io/ioutil"
+	"log"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,18 +12,81 @@ import (
 	"github.com/gosimple/slug"
 )
 
-type Topic struct {
+type Node struct {
+	Parent     *Node
+	Children   []*Node
 	Name       string
+	Title      string
 	Identifier string
-	Entries    []Entry
+	Path       string
+
+	indent int
 }
 
-type Entry struct {
-	Title        string
-	RelativePath string
+func (n *Node) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(strings.Repeat("  ", n.indent) + n.Name)
+	out.WriteString("\n")
+
+	for _, v := range n.Children {
+		out.WriteString(v.String())
+	}
+
+	return out.String()
+}
+
+func BuildContext(path string, root *Node) []*Node {
+	dirs, err := ioutil.ReadDir(path)
+	if err != nil {
+		panic(err)
+	}
+
+	if len(dirs) == 0 {
+		log.Fatalf("Folder '%s' has no contents to process.", path)
+	}
+
+	children := make([]*Node, 0)
+
+	for _, v := range dirs {
+		ext := filepath.Ext(filepath.Join(path, v.Name()))
+		if !v.IsDir() && ext != ".md" {
+			continue
+		}
+
+		node := &Node{}
+		node.Parent = root
+		node.Children = nil
+		node.Name = strings.Title(v.Name())
+		node.Title = getTitleFromFileName(v)
+		node.Identifier = slug.Make(v.Name())
+		node.Path = getRelativePath(path, v)
+		node.indent = root.indent + 1
+
+		if v.IsDir() {
+			node.Children = BuildContext(filepath.Join(path, v.Name()), node)
+		}
+
+		children = append(children, node)
+	}
+
+	return children
 }
 
 func main() {
+	root := Node{}
+	root.Parent = nil
+	root.Name = "markdown"
+	root.Identifier = "root"
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	markdownFolder := filepath.Join(cwd, "../", "markdown")
+	root.Children = BuildContext(markdownFolder, &root)
+
 	template, err := template.ParseFiles("README.template")
 	if err != nil {
 		panic(err)
@@ -30,59 +95,10 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = template.Execute(file, EnumerateTopics())
+	err = template.Execute(file, root)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func EnumerateTopics() []Topic {
-	topics := make([]Topic, 0)
-
-	cwd, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	markdownFolder := filepath.Join(cwd, "../", "markdown")
-
-	dirs, err := ioutil.ReadDir(markdownFolder)
-	if err != nil {
-		panic(err)
-	}
-
-	if len(dirs) == 0 {
-		panic("No entries found to use for generation.")
-	}
-
-	for _, v := range dirs {
-		if ignoreProjectFolders(v) || !v.IsDir() {
-			continue
-		}
-
-		topic := Topic{
-			Name:       strings.Title(v.Name()),
-			Identifier: slug.Make(v.Name()),
-		}
-
-		topicDir, err := ioutil.ReadDir(filepath.Join(markdownFolder, v.Name()))
-		if err != nil {
-			panic(err)
-		}
-
-		for _, t := range topicDir {
-			extension := filepath.Ext(filepath.Join(markdownFolder, v.Name(), t.Name()))
-			if extension == ".md" {
-				entry := Entry{
-					Title:        getTitleFromFileName(t),
-					RelativePath: filepath.Join("markdown", v.Name(), t.Name()),
-				}
-				topic.Entries = append(topic.Entries, entry)
-			}
-		}
-		topics = append(topics, topic)
-	}
-	return topics
 }
 
 func ignoreProjectFolders(file os.FileInfo) bool {
@@ -101,4 +117,15 @@ func getTitleFromFileName(file os.FileInfo) string {
 	name := strings.TrimSuffix(file.Name(), filepath.Ext(file.Name()))
 	name = strings.Replace(name, "-", " ", -1)
 	return strings.Title(name)
+}
+
+func getRelativePath(parentDirectory string, file os.FileInfo) string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	rootDir := filepath.Join(pwd, "../")
+	fullPath := filepath.Join(parentDirectory, file.Name())
+	return strings.ReplaceAll(fullPath, rootDir+"/", "")
 }
